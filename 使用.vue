4249940b8db7bb9ff2d42a4752d08cecc226670d
大萌 <65117107+mengmeng-lab@users.noreply.github.com>
2022-04-06 16:50:26 +0800

    Create 使用.vue

---
+++ 使用.vue
@@ -0,0 +1,19 @@
+<!--system.vue-->
+<template>
+      <colorPicker v-model="color" @change="handleChangeColor"></colorPicker>
+</template>
+<script>
+
+export default {
+  name: 'system',
+  data () {
+    return {
+      color: #000;
+    }
+  },
+  methods:{
+   handleChangeColor (val) {
+      console.log('输出颜色值',val)  // 颜色 #6c8198
+   },
+  }
+</script>
